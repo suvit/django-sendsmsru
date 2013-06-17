aa56791337a0fac6f40c045f6de1c536ad6b31a5
Victor Safronovich <vsafronovich@gmail.com>
2013-06-17 11:10:09 +0600

    fixed installer

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@
 # This file is part of sendsms released under the MIT license. 
 # See the NOTICE for more information.
 
-import os
-import sys
 from setuptools import setup, find_packages
 
-from sendsmsru import VERSION
-
+reqs = ['django',
+        'django-sendsms']
 try:
     import importlib
 except ImportError:
@@ -16,21 +14,15 @@ except ImportError:
 
 setup(
     name='django-sendsmsru',
-    version=VERSION,
+    version=__import__('sendsmsru').VERSION,
     description='Russian backends for django-sendsms',
-    long_description=file(
-        os.path.join(
-            os.path.dirname(__file__),
-            'README.md'
-        )
-    ).read(),
+    long_description=open('README.md').read(),
     author='Victor Safronovich',
     author_email='vsafronovich@gmail.com',
     license='MIT',
     url='http://github.com/suvit/django-sendsmsru',
     zip_safe=False,
     packages=find_packages(exclude=['docs', 'examples', 'tests']),
-    install_requires=['django',
-                      'django-sendsms'],
+    install_requires=reqs,
     include_package_data=True,
 )
